5665844e5785d1c7ce44e3f16cf40356b50ce7b4
jsg <5577598+apollolabsai@users.noreply.github.com>
2026-01-25 08:37:09 -0800

    Increase media API page size to 2000

--- flask_app/services/media_service.py
+++ flask_app/services/media_service.py
@@ -219,7 +219,7 @@ class MediaService:
             server_name: Server name for progress updates
         """
         status = self.get_or_create_status()
-        page_size = 500
+        page_size = 2000
         start = 0
         total_records = None
 
